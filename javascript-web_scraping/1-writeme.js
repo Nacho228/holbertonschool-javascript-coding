b389f746e81ccaddb54f48a58c613433bc988e55
Nacho228 <6319@holbertonstudents.com>
2023-09-24 09:18:51 -0700

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,10 +3,8 @@ const fs = require('fs');
 const filepath = process.argv[2];
 const str = process.argv[3];
 
-fs.writeFile(filepath, str, 'utf-8', (err, data) => {
+fs.writeFile(filepath, str, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(data);
   }
 });
