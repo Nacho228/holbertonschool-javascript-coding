03cc10a5e62834258882c640a5708fe5bab42dfe
Nacho228 <6319@holbertonstudents.com>
2023-09-24 09:02:44 -0700

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs')
-const filepath = process.argv[2]
+const fs = require('fs');
+const filepath = process.argv[2];
 
 fs.readFile(filepath, 'utf-8', (err, data) => {
     if (err) {
-        console.error(err)
-    }
-    else {
-        console.log(data)
+        console.error(err);
+    } else {
+        console.log(data);
     }
 });
